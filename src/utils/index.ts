33409a01f3c1af7c343afa1d2fefaa5db8d80d8e
Adrien Lepoutre <thewebsdoor@gmail.com>
2020-11-09 12:53:01 -0500

    fix: default image facebook (#7)
    fix: content

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,14 +11,14 @@ const baseSiteURL = 'https://aidenclarkmusic.com';
 
 export const updateMetas = ({ title, description, path }: IUpdateMetasProps) => {
   const pathParts = path.split('/');
-  const pageSlug = pathParts[pathParts.length - 1] || 'default';
+  const imageName = pathParts[pathParts.length - 1] ? pathParts[pathParts.length - 1] + '.jpg' : 'default.png';
 
   document.title = config.title + title;
 
   document.getElementById('og_title').setAttribute('content', config.title + title);
   document.getElementById('og_description').setAttribute('content', description);
   document.getElementById('meta_description').setAttribute('content', description);
-  document.getElementById('og_image').setAttribute('content', `${baseSiteURL}/facebook/${pageSlug}.jpg`);
+  document.getElementById('og_image').setAttribute('content', `${baseSiteURL}/facebook/${imageName}`);
   document.getElementById('og_url').setAttribute('content', `${baseSiteURL}${path}`);
 };
 
